546da3b0e45ed0577c5111e4e1b57f58e42df027
David Lago <davidlago@gmail.com>
2015-06-06 15:07:21 -0400

    Storing answers in session object

--- web-app/routes/questions.js
+++ web-app/routes/questions.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET questions page. */
 router.get('/', function(req, res) {
   var dummy_question = {
     id: 12,
     text: "What is your gender?",
     answer_mode: "radio-buttons",
+    answer_choices: [
+      {
+        id: 1,
+        text: "Male"
+      },
+      {
+        id: 2,
+        text: "Female"
+      }
+    ]
+  }
+  res.render('question', { question: dummy_question });
+});
+
+/* POST answers. */
+router.post('/', function(req, res) {
+  // Save question in session hash variable 'answers':
+  // {
+  //   'question1': 'answer',
+  //   'question2': 'answer2'
+  // }
+  req.session.answers = req.session.answers || {}
+  req.session.answers[req.body.question] = req.body.answer;
+  console.log(req.session.answers);
+  var dummy_question = {
+    id: 12,
+    text: req.body.question,
+    answer_mode: req.body.answer,
     answer_choices: [
       {
         id: 1,

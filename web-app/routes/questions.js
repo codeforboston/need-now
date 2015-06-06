39f470b1b5cdcb6c1d5605e0f5674fd514f6699d
David Lago <davidlago@gmail.com>
2015-06-06 13:35:34 -0400

    Added post to questions route

--- web-app/routes/questions.js
+++ web-app/routes/questions.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET questions page. */
 router.get('/', function(req, res) {
   var dummy_question = {
     id: 12,
     text: "What is your gender?",
     answer_mode: "radio-buttons",
+    answer_choices: [
+      {
+        id: 1,
+        text: "Male"
+      },
+      {
+        id: 2,
+        text: "Female"
+      }
+    ]
+  }
+  res.render('question', { question: dummy_question });
+});
+
+/* POST answers. */
+router.post('/', function(req, res) {
+  var dummy_question = {
+    id: 12,
+    text: req.body.question,
+    answer_mode: req.body.answer,
     answer_choices: [
       {
         id: 1,

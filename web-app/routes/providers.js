3a661dd2262c29689801f8b7c790c9ab96391a54
David Lago <davidlago@gmail.com>
2015-06-07 10:58:28 -0400

    Extracting provider filtering

--- web-app/routes/providers.js
+++ web-app/routes/providers.js
@@ -4,10 +4,27 @@ var router = express.Router();
 // Pre-load list of providers
 var providers = require('../public/data/providers.json');
 
+// Parse answers from session hash into a more readable JSON object
+var parsedAnswers(answers) {
+  // Mocked for now
+  return {
+    gender: "male",
+    age: "under 29",
+    interests: ["medical assistance", "other"]
+  }
+}
+
+// Filter the providers list based on the answers in the session hash
+var filteredProviders(answers) {
+  return providers.slice(0,2);
+}
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  // Just for testing, send the first 2 providers
-  res.render('provider_table', { providers: providers.slice(0,2) });
+  res.render('provider_table', {
+    providers: filteredProviders(req.session.answers),
+    answers: parsedAnswers(req.session.answers)
+  });
 });
 
 module.exports = router;

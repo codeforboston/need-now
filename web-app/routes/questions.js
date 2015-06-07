9416861326329b979dee12865a732dacee073fe8
David Lago <davidlago@gmail.com>
2015-06-07 10:17:17 -0400

    Get next question from unanswered

--- web-app/routes/questions.js
+++ web-app/routes/questions.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var _ = require("underscore");
 
 // Pre-load list of questions
 var questions = require('../public/data/questions.json');
 
+// Figure out which question comes next by comparing the answers hash stored
+// in the session object and the list of questions. Return then first non-
+// answered question or null if they all have responses recorded.
+var nextQuestion = function(answers) {
+  // If we have all the answers, return null
+  if (answers && Object.keys(answers).length == questions.length) return null;
+
+  // If no answers yet, return the first non-answered question and return it
+  if (!answers || answers == {})
+    return questions[0];
+
+  // Otherwise, return the first non-answered question
+  var unanswered = _.filter(questions, function(question) {
+    return !_.contains(Object.keys(answers), String(question.id));
+  });
+
+  // Return first unanswered (or null if for some weird reason we got none
+  if (unanswered.length > 0) return unanswered[0];
+  return null;
+}
+
 /* GET questions page. */
 router.get('/', function(req, res) {
-  // Just for testing, send first one
-  res.render('question', { question: questions[0] });
+  next = nextQuestion(req.session.answers);
+  if (next == null) // No more questions, show providers
+    res.redirect('/providers');
+  else // Render next question
+    res.render('question', { question: next });
 });
 
 /* POST answers. */
@@ -19,14 +44,14 @@ router.post('/', function(req, res) {
   // }
   req.session.answers = req.session.answers || {}
   req.session.answers[req.body.question] = req.body.answer;
+  console.log('Questions so far:');
   console.log(req.session.answers);
-  // Just for testing, if we have 3 diferent answers, go to results
-  if (Object.keys(req.session.answers).length > 3) {
-    res.render('results', { question: dummy_question });
-  } else {
-    // Send the first one, just for testing
-    res.render('question', { question: questions[0] });
-  }
+
+  next = nextQuestion(req.session.answers);
+  if (next == null) // No more questions, show providers
+    res.redirect('/providers');
+  else // Render next question
+    res.render('question', { question: next });
 });
 
 module.exports = router;

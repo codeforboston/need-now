c746f4617d173bbeeb860487ef13d12de3987e76
sdaitzman <samuel@daitzman.com>
2015-06-15 11:57:01 -0400

    fixes stylesheet name

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('sass', function() {
         'android 4'
       )
     )
-  .pipe(rename({suffix: 'min'}))
+  .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('./public/stylesheets'))
   .pipe(notify({message: 'Sass has been compiled'}));
 });
